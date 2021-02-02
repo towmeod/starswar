c3b5f95885fe60fab8dc6e42662cf6eb07ba0938
Andrew <77741677+towmeod@users.noreply.github.com>
2021-02-02 22:00:28 +0200

    Delete src directory

--- src/macros/evaluate.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    'apply': (s) => {
-        return eval(s);
-    }
-};
--- src/macros/nomangle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const split = require('../util/split');
-
-module.exports = {
-    'apply': (s) => {
-        return split.START_TAG + s + split.END_TAG;
-    }
-};
